89e5c8c7855d8fd85ccb611da8579bffa7895128
hoangtuananh <tuanhoanga789@gmail.com>
2025-10-27 00:58:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import TodoList from "./components/TodoList";
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import { useCallback, useState } from "react";
+import { v4 } from "uuid";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [textInput, setTextInput] = useState("");
+
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value);
+  }, []);
+
+  const onAddBtnClick = useCallback((e) => {
+    setTodoList([...todoList, { id: v4(), name: textInput, isCompleted: false }]);
+  }, [todoList, textInput]);
+
+  return (
+    <>
+      <h3>Danh sach can lam</h3>
+      <Textfield
+        name="add-todo"
+        placeholder="Nhap cong viec can lam"
+        elemAfterInput={
+          <Button isDisabled={!textInput} appearance="primary"
+            onClick={onAddBtnClick}
+          >
+            Them
+          </Button>
+        }
+        css={{ padding: "2px 4px 2px" }}
+        value={textInput}
+        onChange={onTextInputChange}
+      ></Textfield>
+      <TodoList todoList={todoList} />
+    </>
+
+  )
+}
+
+export default App;
+
+// Chửa hiểu đoạn useCallback
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Code gốc của file này khác với code gốc trong video
